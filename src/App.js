029d25fb4b4b48a2e162ca4bee37cdf03fe44fc4
shuaizhao1988 <69214951+shuaizhao1988@users.noreply.github.com>
2020-10-18 14:50:35 -0400

    added Github repositry URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was created by Shuai Zhao and is{" "}
+        <a
+          href="https://github.com/shuaizhao1988/final-react-weather-app"
+          target="_blank"
+        >
+          Open-sourced on Gitbhub
+        </a>
+      </footer>
+    </div>
+  );
 }
