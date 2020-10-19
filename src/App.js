6862521cfcca3512b48ae5b4691e26252ea697a3
shuaizhao1988 <69214951+shuaizhao1988@users.noreply.github.com>
2020-10-19 16:54:01 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was created by Shuai Zhao and is{" "}
-        <a
-          href="https://github.com/shuaizhao1988/final-react-weather-app"
-          target="_blank"
-        >
-          Open-sourced on Gitbhub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was created by Shuai Zhao and is{" "}
+          <a
+            href="https://github.com/shuaizhao1988/final-react-weather-app"
+            target="_blank"
+          >
+            Open-sourced on Gitbhub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
