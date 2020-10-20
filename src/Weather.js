708c57f2f74b854e897fcad37d9dd8223f3f602d
shuaizhao1988 <69214951+shuaizhao1988@users.noreply.github.com>
2020-10-19 20:40:46 -0400

    added the basic styling of the App

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
             type="search"
             placeholder="Type a city..."
             className="form-control"
+            autoFocus="on"
           />
         </div>
         <div className="col-3">
-          <input type="submit" value="Search" className="btn btn-primary" />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
         </div>
       </div>
       <h1>New York</h1>
@@ -21,13 +26,19 @@ export default function Weather() {
         <li>Tuesday 5:04</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mn-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          18ºC
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">18</span>
+              <span className="unit">ºC</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
